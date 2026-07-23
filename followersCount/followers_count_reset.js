d9d98c9a23ba14c5bfe7f328ff87975bd291bfe3
tams302 <tams302@gmail.com>
2026-07-23 19:43:50 -0400

    Add reset button

---
+++ followersCount/followers_count_reset.js
@@ -0,0 +1,25 @@
+let count = 0; // Initialize count to 0
+
+function increaseCount() {
+  count++; // Increment the count by 1
+  displayCount(); // Display the count
+  checkCountValue(); // Check count value and display messages
+}
+
+function checkCountValue() {
+  if (count === 10) {
+    alert("Your Instagram post gained 10 followers! Congratulations!");
+  } else if (count === 20) {
+    alert("Your Instagram post gained 20 followers! Keep it up!");
+  }
+}
+
+function resetCount() {
+  count = 0;
+  displayCount(); // Reset the HTML view back to 0
+  alert("Your follower count has been reset.");
+}
+
+function displayCount() {
+document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
+}
